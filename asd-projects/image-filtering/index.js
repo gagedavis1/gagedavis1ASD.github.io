4e86ad745cb46315e14f180c6d9c409f1c069776
gagedavis1 <142328369+gagedavis1@users.noreply.github.com>
2025-02-05 03:38:19 +0000

    finished up image filtering :B

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,8 +20,12 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  applyFilterNoBackground(reddify);
+  keepInBounds();
+  applyFilterNoBackground(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
 
-  
 
   // do not change the below line of code
   render($("#display"), image);
@@ -32,18 +36,72 @@ function applyAndRender() {
 /////////////////////////////////////////////////////////
 
 // TODO 1, 2 & 4: Create the applyFilter function here
+function applyFilter(filterFunction){
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+      var rgbString = image[i][j]
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      //rgbNumbers[GREEN] = 0
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
 
+    }
+  }
+}
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction){
 
+  var backgroundColor = rgbStringToArray(image[0][0])
+  
+  for (var i = 0; i < image.length; i++) {
+    for (var j = 0; j < image[i].length; j++) {
+    var rgbString = image[i][j]
+    var rgbNumbers = rgbStringToArray(rgbString)
+      if (!arraysEqual(rgbNumbers, backgroundColor)){
+        filterFunction(rgbNumbers);
+      }
+
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString
+    }
+  }
+}
+
+function arraysEqual(arr1, arr2){
+  return arr1[RED] === arr2[RED] && arr1[GREEN] === arr2[GREEN]
+}
+
+function applyAndRender(){
+  applyFilterNoBackground(reddify);
+  keepInBounds();
+  applyFilterNoBackground(reddify);
+  applyFilterNoBackground(decreaseBlue);
+  applyFilterNoBackground(increaseGreenByBlue);
+
+  //render the last bit (the image)
+  render($("#display"), image);
+}
 
 // TODO 5: Create the keepInBounds function
-
+function keepInBounds(x){
+  return x > 255 ? 255 : x < 0 ? 0 : x ;
+}
 
 // TODO 3: Create reddify function
-
+function reddify(arr){
+  arr[RED] = 200;
+}
 
 // TODO 6: Create more filter functions
+function decreaseBlue(arr2){
+  arr2[BLUE] = keepInBounds(arr2[BLUE] - 50);
+}
 
-
+function increaseGreenByBlue(arr2){
+  arr2[GREEN] = keepInBounds(arr2[GREEN] + arr2[BLUE]);
+}
 // CHALLENGE code goes below here
+
+//Nah im good :)
